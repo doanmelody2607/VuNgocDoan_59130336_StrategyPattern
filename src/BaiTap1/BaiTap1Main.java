772483ccbf8c7bca77dd01c7e847f50e67ad661d
doanmelody2607 <63444961+doanmelody2607@users.noreply.github.com>
2020-04-22 08:12:12 +0700

    v1.2

--- src/BaiTap1/BaiTap1Main.java
+++ src/BaiTap1/BaiTap1Main.java
@@ -14,8 +14,16 @@ public class BaiTap1Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
+        Context context = new Context();
+        //Thuc hien phep cong
+        context.setTinhToan(new Cong());
+        System.out.println("75 + 12 = " + context.tinh(75, 12));
+        //Thuc hien phep tru
+        context.setTinhToan(new Tru());
+        System.out.println("54 - 78 = " + context.tinh(54, 78));
     }
     
 }
